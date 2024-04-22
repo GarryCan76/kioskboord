5f32861214cbb3bbafa265005cfd5a8edad6f362
stijn <stijn@users.noreply.github.com>
2024-04-22 21:19:56 +0200

    alles gaat goed [:

--- client/js/index.js
+++ client/js/index.js
@@ -1,14 +1,44 @@
 const socket = io();
-import Jsml from './jsml.js';
+import Jsml from "./jsml.js";
 const jsml = new Jsml();
-socket.on('connect', ()=> {
-    socket.on('productsItems', productItems=>{
-        let beef = productItems.drinks;
+let productlist = false;
+socket.on("connect", () => {
+  document.getElementById("drinks").addEventListener("click", () => {
+    page("drinks");
+  });
+  document.getElementById("beef").addEventListener("click", () => {
+    page("beef", "burgers");
+  });
+  document.getElementById("fish").addEventListener("click", () => {
+    page("fish", "burgers");
+  });
+  document.getElementById("chicken").addEventListener("click", () => {
+    page("chicken", "burgers");
+  });
+  document.getElementById("fries").addEventListener("click", () => {
+    page("fries");
+  });
+  socket.on("productsItems", (productItems) => {
+    productlist = productItems;
+    // beef = productItems.burgers.beef;
+  });
+});
 
-        beef.map(beefBurger=>{
-            jsml.createHTMLElement('img', document.body, false, {'src':beefBurger.image})
-            jsml.createHTMLElement('p', document.body, beefBurger.name)
-            jsml.createHTMLElement('p', document.body, beefBurger.Kcal)
-        })
-    })
-})
+function page(pageType, productroot) {
+  let products = false;
+  if (productroot) {
+    products = productlist[productroot][pageType];
+  } else {
+    products = productlist[pageType];
+  }
+  
+  products.map((product) => {
+    console.log(product);
+    // products.map(product, () => {
+    //     jsml.createHTMLElement('img', document.getElementById('items'), false, {'src':beefBurger.image, "classList": 'item_image'})
+    //     jsml.createHTMLElement('p', document.getElementById('items'), beefBurger.name, {"classList":"item_product_name"})
+    //     jsml.createHTMLElement('p', document.getElementById('items'), beefBurger.Kcal, {"classList":"item_kcal"})
+    //     jsml.createHTMLElement('p', document.getElementById('items'), '€' + beefBurger.price,  {"classList":"item_price"})
+    // })
+  });
+}
